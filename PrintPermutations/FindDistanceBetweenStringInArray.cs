97edbb7185d722b490609f814a6f8825443ed300
ajay anand <ajayanandsharma@gmail.com>
2015-08-28 13:58:26 +0530

    check-in

---
+++ PrintPermutations/FindDistanceBetweenStringInArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class FindDistanceBetweenStringInArray
+    {
+        public int FindDis(String[] strArray, string a, string b)
+        {
+            if (strArray == null)
+            {
+                throw new ArgumentNullException("strArray");
+            }
+
+            if (strArray.Length <= 0)
+            {
+                throw new ArgumentException("strArray");
+            }
+
+            if (string.IsNullOrEmpty(a))
+            {
+                throw new ArgumentNullException("a");
+            }
+
+            if (string.IsNullOrEmpty(b))
+            {
+                throw new ArgumentNullException("b");
+            }
+
+            int minDis = int.MaxValue;
+            int prev = -1;
+            for (int index = 0; index < strArray.Length; index++)
+            {
+                if (strArray[index].Equals(a, StringComparison.OrdinalIgnoreCase) ||
+                    strArray[index].Equals(b, StringComparison.OrdinalIgnoreCase))
+                {
+                    prev = index;
+                    break;
+                }
+            }
+
+            for (int index = prev; index < strArray.Length; index++)
+            {
+                if (strArray[index].Equals(a, StringComparison.OrdinalIgnoreCase) ||
+                    strArray[index].Equals(b, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (!strArray[prev].Equals(strArray[index], StringComparison.OrdinalIgnoreCase)
+                        && (index - prev) < minDis)
+                    {
+                        minDis = index - prev;
+                        prev = index;
+                    }
+                    else
+                    {
+                        prev = index;
+                    }
+                }
+
+            }
+
+            return minDis == int.MaxValue ? -1 : minDis;
+        }
+    }
+}
---
+++ PrintPermutations/StringPermutations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class StringPermutations
+    {
+
+        // Algorithm Paradigm: Backtracking
+        // Time Complexity: O(n*n!)
+        public StringPermutations()
+        {
+             // start program
+            char[] str = "ABC".ToCharArray();
+            int n = str.Length;
+            permute(str, 0, n - 1);
+        }
+
+        public static void swap(char[] str, int x, int y)
+        {
+            char temp;
+            temp = str[x];
+            str[x] = str[y];
+            str[y] = temp;
+        }
+
+        /* Function to print permutations of string
+   This function takes three parameters:
+   1. String
+   2. Starting index of the string
+   3. Ending index of the string. */
+        public static void permute(char[] str, int s, int e)
+        {
+            int i;
+
+            if (s == e)
+            {
+                Console.WriteLine("{0}", new string(str));
+            }
+            else
+            {
+                for (i = s; i <= e; i++)
+                {
+                    swap(str, s, i);
+                    permute(str, s + 1, e);
+                    swap(str, s, i);
+                }
+            }
+        }
+    }
+}
