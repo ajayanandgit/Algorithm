9df4627d363035cd19471cdc0148425ec61d1c43
ajayanandgit <ajayanandsharma@gmail.com>
2015-08-31 12:17:12 +0530

    added code for finding hole in given integer

--- PrintPermutations/Program.cs
+++ PrintPermutations/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +16,7 @@ namespace Algorithm
             // var fileData = "wlfr2r09032!#$%^*())___+{}:?><~";
 
             var fileData = File.ReadAllText(@"C:\Ajay - Imp\test.txt");
-            var temp = Fix(fileData);
+            // var temp = Fix(fileData);
             byte[] bytes = Encoding.UTF8.GetBytes(fileData);
             var tt = bytes.Aggregate(string.Empty, (current, t) => current + (char)t);
 
@@ -32,8 +34,35 @@ namespace Algorithm
             int m = coins.Length;
             int V = 11;
             var minCoin = CoinChangeProblem.MinCoins(coins, m, V);
-
+            int tets  = solvePuzzle(640);
             Console.ReadKey();
+        }
+
+        static int solvePuzzle(int num)
+        {
+            // check input
+            if (0 > num || num > Math.Pow(10, 9)) throw new ArgumentException("num");
+            int countHole = 0;
+            while(num > 0)
+            {
+                int rem = num%10;
+                switch (rem)
+                {
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 0:
+                        countHole++;
+                        break;
+                    case 8:
+                        countHole += 2;
+                        break;
+                }
+
+                num = num/10;
+            }
+
+            return countHole;
         }
 
 
